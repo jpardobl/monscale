7a26fc4ff330d20438aab3412f8d8ebbabb86ba8
karmab <karimboumedhel@gmail.com>
2014-05-05 16:31:46 +0200

    suds version of cloudforms.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'monscale',
-    version = '0.7',
+    version = '0.10',
     packages = ["monscale"],
     include_package_data = False,
     license = 'BSD License',
